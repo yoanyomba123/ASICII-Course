7344ff884f2fbf94b5c6ea187657a1c9c77da7a6
nickjoseph16 <45366035+nickjoseph16@users.noreply.github.com>
2019-06-01 17:48:38 -0400

    Update dc_synth.tcl

--- dc_synth/dc_synth.tcl
+++ dc_synth/dc_synth.tcl
@@ -1,16 +1,12 @@
 ## Give the path to the verilog files and define the WORK directory
 #
 set username naj47
-lappend search_path /home/${username}@drexel.edu/ASICII-Course/tutorial/OpenSPARC/trunk/T1-FPU/  
+lappend search_path /home/${username}@drexel.edu/ASICII-Course/tutorial/OpenSPARC/trunk/T1-FPU/
 
 define_design_lib WORK -path "work"
 #
 ### Define the library location
-set link_library [ list /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/stdcell_lvt/db_ccs/saed32lvt_ss0p95v25c.db /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/sram/db_ccs/saed32sram_ss0p95v25c.db] 
-#/mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/stdcell_lvt/db_ccs/saed32lvt_ss0p95v25c.db 
-#/mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/stdcell_rvt/db_ccs/saed32rvt_ss0p95vn40c.db 
-#/mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/sram/db_ccs/saed32sram_ss0p95v125c.db 
-#/mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/sram/db_ccs/saed32sram_ss0p95v25c.db 
+set link_library [ list /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/stdcell_lvt/db_ccs/saed32lvt_ss0p95v25c.db /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/sram/db_ccs/saed32sram_ss0p95v25c.db /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/stdcell_lvt/db_ccs/saed32lvt_ss0p95v125c.db /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/stdcell_lvt/db_ccs/saed32lvt_ss0p95vn40c.db /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/sram/db_ccs/saed32sram_ss0p95v125c.db]
 #/mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/sram/db_ccs/saed32sram_ss0p95vn40c.db ]
 
 set target_library [ list /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/stdcell_lvt/db_ccs/saed32lvt_ss0p95v25c.db /mnt/class_data/ecec574-w2019/PDKs/SAED32nm_new/SAED32_EDK/lib/sram/db_ccs/saed32sram_ss0p95v25c.db]
@@ -31,7 +27,7 @@ set default_clk gclk
 reset_design
 
 #Create clock 
-create_clock -name ideal_clock -period 90 [get_ports gclk] 
+create_clock -name ideal_clock -period 90 [get_ports gclk]
 
 #Set Clock Constraints
 set_clock_uncertainty 0.1 [get_clocks ideal_clock]
@@ -46,7 +42,7 @@ set_load 0.3 [all_outputs]
 ### Compilation
 ### you can change medium to either low or high
 # compile -area_effort medium -map_effort medium -power_effort medium
-compile_ultra -gate_clock -retime 
+compile_ultra -gate_clock -retime
 
 
 ### Below commands report area , cell, qor, resources, and timing information needed to analyze the design.
@@ -58,7 +54,7 @@ report_resources > reports/synth_resources.rpt
 report_timing -max_paths 100 > reports/synth_timing.rpt
 # Dump out the constraints in an SDC file
 write_sdc  const/fpu.sdc
-
+      
 ## Dump out the synthesized database and gate-level-netlist
 write -f ddc -hierarchy -output output/fpu.ddc
 write -hierarchy -format verilog -output  output/fpu.v
